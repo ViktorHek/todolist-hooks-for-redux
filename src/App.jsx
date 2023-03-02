9a137675c6350e004c99be9a91eb43ebd83206fa
viktorHek <viktorkarlsson125@gmail.com>
2023-03-02 11:33:27 +0100

    done. has improvement potensial

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,51 @@
 import React, { useState } from "react";
-// import { ToDoItem } from "./ToDoItem";
 import { addTodo, checkTodo, removeTodo, useTodo } from "./redux/store";
 
 const App = () => {
-  // const list = useList();
-  // const [text, setText] = useState("");
+  const [text, setText] = useState("");
+  const [render, setRender] = useState(false);
 
-  // const createNewToDoItem = () => {
-  //   if (!text) return alert("Please enter text!");
-  //   addItem({ text });
-  //   setText("");
-  // };
+  function handleChange(event) {
+    event.preventDefault();
+    setText(event.target.value);
+  }
+
+  function handleClick() {
+    let obj = {
+      text: text,
+      check: false,
+    };
+    addTodo(obj);
+    setText("");
+  }
+
+  function handleCheck(text) {
+    checkTodo(text);
+    setRender(!render);
+  }
+
+  let displayLis = useTodo().map((el) => {
+    return (
+      <p
+        key={el.text}
+        onClick={() => handleCheck(el.text)}
+        style={{ color: `${el.check ? "green" : "red"}` }}>
+        {el.text}
+      </p>
+    );
+  });
 
   return (
-    <div>
-      <h1>{useTodo()}</h1>
-      <a href="#" onClick={() => addTodo("shitting")}>
-        add
-      </a>
+    <div className="main-container">
+      <div className="list-container">{displayLis && displayLis}</div>
+      <div>
+        <input onChange={(event) => handleChange(event)} value={text}></input>
+        <button onClick={() => handleClick()}>submit</button>
+      </div>
       <a href="#" onClick={() => removeTodo()}>
         remove
       </a>
-      {/* <ul>
-        {list.map((item) => (
-          <ToDoItem key={item.id} item={item} />
-        ))}
-      </ul>
-
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyPress={(e) => e.key === "Enter" && createNewToDoItem()}
-      />
-      <button onClick={createNewToDoItem}>{" + "}</button> */}
     </div>
   );
 };
-export default App
+export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,23 @@
-import { useRedux } from "hooks-for-redux";
-const initList = "do shit";
+import { createReduxModule } from "hooks-for-redux";
+const initList = [{ text: "doing shit", check: false }];
+const mainList = [{ text: "doing shit", check: false }];
 
-export const [useTodo, { addTodo, checkTodo, removeTodo }] = useRedux("bja", initList, {
-  addTodo: (store, todo) => todo,
+export const [useTodo, { addTodo, checkTodo, removeTodo }] = createReduxModule("todo", mainList, {
+  addTodo: (store, todo) => handleAddTodo(todo),
   removeTodo: () => initList,
-  checkTodo: () => initList,
+  checkTodo: (store, val) => handleCheckTodo(val),
 });
+
+function handleAddTodo(todo) {
+  mainList.push(todo);
+  return mainList;
+}
+
+function handleCheckTodo(val) {
+  mainList.map((el) => {
+    if (el.text === val) {
+      el.check = !el.check;
+    }
+  });
+  return mainList;
+}
